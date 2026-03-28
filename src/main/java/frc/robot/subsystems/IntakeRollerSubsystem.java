7c388233d5090e94f70905a509d2396625056eaa
Oldgoer <jacob.newcomer@gmail.com>
2026-03-27 20:23:44 -0400

    IntakeRollers Talon to SparkMax - RB

--- src/main/java/frc/robot/subsystems/IntakeRollerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeRollerSubsystem.java
@@ -7,6 +7,13 @@ import static edu.wpi.first.units.Units.Volts;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,31 +21,38 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeRollerSubsystem extends SubsystemBase
 {
-  public static final double kWristMomentOfInertia = 0.00032; // kg * m^2
-
-  private final TalonSRX m_IntakeRollers = new TalonSRX(40);
-
-  //private final DCMotor m_rollerMotorGearbox = DCMotor.getVex775Pro(1);
+  private final SparkMax m_IntakeRollers = new SparkMax(40,MotorType.kBrushless);
 
   public IntakeRollerSubsystem()
   {
-    // Reset the Talon SRX to factory defaults and set neutral (idle) mode to Coast.
-    m_IntakeRollers.configFactoryDefault();
-    m_IntakeRollers.setNeutralMode(com.ctre.phoenix.motorcontrol.NeutralMode.Coast);
-    m_IntakeRollers.setInverted(false);
-    m_IntakeRollers.enableCurrentLimit(true);
-    m_IntakeRollers.configContinuousCurrentLimit(40);
-    m_IntakeRollers.configPeakCurrentLimit(40);
-    // Optionally configure current limits or inversion here as needed.
+    SparkMaxConfig config = new SparkMaxConfig();
+    config
+        .inverted(false)
+        .smartCurrentLimit(100)
+        .idleMode(IdleMode.kCoast);
+    m_IntakeRollers.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public Command setIntakeRoller(double speed)
   {
     return run(() -> {
-      m_IntakeRollers.set(ControlMode.PercentOutput, speed);
+      m_IntakeRollers.set(speed);
     });
   }
 
+  public void intake(double speed){
+    m_IntakeRollers.set(speed);
+  }
+
+  public void outake(double speed){
+    m_IntakeRollers.set(-speed);
+  }
+
+  public void STOP(){
+    m_IntakeRollers.set(0);
+  }
+
+  
   public Command out(double speed)
   {
     return setIntakeRoller(-speed);
@@ -53,32 +67,4 @@ public class IntakeRollerSubsystem extends SubsystemBase
     return setIntakeRoller(0);
   }
 
-  public Voltage getVoltage()
-  {
-    return Volts.of(m_IntakeRollers.getMotorOutputPercent());
-  }
-
-  public boolean outtaking()
-  {
-    if(getCurrentCommand() != null)
-        return getDutycycle() > 0.0 || getCurrentCommand().getName().equals("Outtake");
-    return getDutycycle() > 0.0;
-  }
-
-  public double getDutycycle()
-  {
-    return m_IntakeRollers.getMotorOutputPercent();
-  }
-
-  public void intake(double speed){
-    m_IntakeRollers.set(ControlMode.PercentOutput, speed);
-  }
-
-  public void outake(double speed){
-    m_IntakeRollers.set(ControlMode.PercentOutput, -speed);
-  }
-
-  public void STOP(){
-    m_IntakeRollers.set(ControlMode.PercentOutput, 0);
-  }
 }
